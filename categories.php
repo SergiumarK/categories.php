b0c05320915dd7e6667dffc5d866ffab0c3f651a
SergiumarK <s.marcocimd@gmail.com>
2023-12-19 22:11:11 +0300

    added_a_category_in_db

--- categories.php
+++ categories.php
@@ -1,6 +1,18 @@
 <!-- pagina de lucru cu categorii -->
 <?php
     require_once "config.php";
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if ($_POST["type"] === "category") {
+            $name = $_POST["name"];
+            $sql = "INSERT INTO categories(name) VALUES('$name')";
+            IF ($conn->query($sql) ===true) {
+                $categoryMessage = "Category created successfully";
+             } else {
+                $categoryMessage = "Category not created";
+             }
+        }
+    }
 ?>
 
 
@@ -12,6 +24,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <h1>Categories</h1>
+    </header>
+    <main>
+        <form action="" method="post">
+            <input type="text" name="name" placeholder="Category name" required>
+            <button name="type" value="category">Create category</button>
+        </form>
+
+        <?php
+            if (isset($categoryMessage)) {
+                echo "<p>$categoryMessage</p>";
+            }
+        ?>
+    </main>
 </body>
 </html>
